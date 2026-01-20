fdd80538220a4c33803a637657aab9be94d042ee
Raphael Holzer <raphael.holzer@citycable.ch>
2026-01-20 16:46:45 +0100

    finish tutorial2d

--- 4_jeu3d/tutoriel2d/main.gd
+++ 4_jeu3d/tutoriel2d/main.gd
@@ -3,18 +3,24 @@ extends Node
 @export var mob_scene: PackedScene
 var score
 
-func _ready():
-	new_game()
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
+	
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
@@ -43,6 +49,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
